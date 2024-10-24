f4c38d05381664820b85be909886cd4c01f53026
Adilson Xavier Fernando <adilsonxavierfernando@gmail.com>
2024-10-24 15:48:56 +0100

    Adicionar arquivos de projeto.

---
+++ TemplateHtmlCSSCSharp/Models/ModelIMC.cs
@@ -0,0 +1,46 @@
+﻿namespace TemplateHtmlCSSCSharp.Models
+{
+    public class ModelIMC
+    {
+        decimal _imc;
+        string _descricao;
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+        public decimal IMC 
+        {
+            get
+            {
+                // Calcular o IMC somente se a altura for maior que zero
+                if (Altura > 0)
+                {
+                    _imc = Peso / (Altura * Altura);
+                }
+                return _imc;
+            }
+        }
+        public string Descricao 
+        {
+            get
+            {
+           
+                if (IMC < 18.5m)
+                {
+                    _descricao = "Abaixo do Peso";
+                }
+                else if (IMC >= 18.5m && IMC < 25m)
+                {
+                    _descricao = "Peso Normal";
+                }
+                else if (IMC >= 25m && IMC < 30m)
+                {
+                    _descricao = "Sobrepeso";
+                }
+                else
+                {
+                    _descricao = "Obesidade";
+                }
+                return _descricao;
+            }
+        }
+    }
+}
